18a0e879e7611ebc0217e99ee093a6b47815af13
gb1175 <gb1175@ILCDTL01gb1175.ITServices.sbc.com>
2017-02-19 15:28:20 -0600

    Moved windows code and forms code out to a library to manage complexity.

---
+++ assets/js/chat-demo-form-manager.js
@@ -0,0 +1,97 @@
+
+/**
+* Contains the for elements to manage.
+* expects to be passed an array of objects where each objects 
+* has the follow properties:
+* 	rootId - the id of the control to manage
+* 	noErrors - the status of errors on this input
+* 	errStatement - the error statement to show.
+* 	Here's an example object: {"rootId":"pcs-first-btn","noErrors":true,"errStatement":"Please insert a first name"}
+*/ 
+var formElements = [];
+
+/**
+* When true, means that the header content in the agent chooser has
+* already been presented to the user. If they had a screen reader running,
+* it would have read the full header, and then be removed.
+*/
+var acHeaderAlreadyRead = false;
+	    
+function formMgr_initialize(configObj) {
+	
+	formElements = configObj;
+}
+
+/**
+* Handle form validation here.
+*/
+function validatePCS() {
+	
+	var hasErrors = false;
+	
+	for (var i in formElements) {
+		if (!formHasValidContent(formElements[i])) {
+			hasErrors = true;
+			buildErrStatement(formElements[i]);
+		}
+	}
+	
+	if (hasErrors) focusOnFirstError();
+}
+
+/**
+ * Focus on the first item in error, if any
+ * 
+ * @returns void
+ */
+function focusOnFirstError() {
+	var len = formElements.length;
+	
+	for (var i=0;i<len;i++) {
+		if (!formElements[i].noErrors) {
+			focusOnItem(jQuery("#"+formElements[i].rootId));
+			break;
+		}
+	}
+}
+
+function formHasValidContent(item) {
+	var result = true;
+	
+	var formItemJq = jQuery("#" + item.rootId);
+	if (formItemJq.length == 0) {
+		ATM.log("Code problem! Can't find the label id '" + item.rootId + "'");
+		return false;
+	}
+	
+	if (formItemJq.val() == "" || formItemJq.val().length < 2) {
+		result = false;
+	}
+	
+	if (!result) item.noErrors = false;	    	
+	return result;
+		
+}
+
+function buildErrStatement(item) {
+	var err = "<div id='"
+	var err2 = "' style='color:red;'>"
+	var err3 = "</div>";
+	var temp;
+    	
+	var result = false;
+	var labelJq = jQuery("#label-" + item.rootId);
+	
+	temp = err + "err-" + item.rootId + err2 + item.errStatement + err3; 
+	labelJq.after(temp);
+	
+	jQuery(item.rootId).attr("aria-describedby","err-" + item.rootId);
+}
+
+function clearErrStatements() {
+	
+	for (var i in formElements) {
+    	jQuery("#err-"+formElements[i].rootId).remove();
+    	jQuery("#" + formElements[i].rootId).removeAttr("aria-describedby");	
+	}
+}
